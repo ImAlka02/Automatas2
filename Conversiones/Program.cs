0253d52a12834efcc7eb069cbd82548d129d8b5f
ImAlka02 <ajiju.2000@hotmail.com>
2023-10-20 14:57:38 -0600

    Agregar archivos de proyecto.

---
+++ Conversiones/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.ComponentModel.Design;
+using System.Linq;
+
+Console.WriteLine("ELige a que quieres convertir tu notacion Infija:");
+Console.WriteLine("1 -- Prefija");
+Console.WriteLine("2 -- Posfija");
+
+Stack pila = new();
+string o = "()-+*/";
+string Resultado = "";
+int opcion = int.Parse(Console.ReadLine());
+string infija = Console.ReadLine();
+
+
+if (opcion == 1) 
+{
+    for (int i = infija.Length-1; i >=0; i--)
+    {
+        if (o.Contains(infija[i]))
+        {
+            if (infija[i] == '(')
+            {
+                Resultado = Resultado + pila.Pop();
+                pila.Pop();
+            }
+            else
+            {
+                pila.Push(infija[i]);
+            }
+        }
+        else
+        {
+            Resultado = Resultado + infija[i];
+        }
+    }
+    if (pila.Count == 1) Resultado = Resultado + pila.Pop();
+
+    Resultado = ReverseString(Resultado);
+} 
+else if( opcion == 2)
+{
+    for (int i = 0; i < infija.Length; i++)
+    {
+        if (o.Contains(infija[i]))
+        {
+            if (infija[i] == ')')
+            {
+                Resultado = Resultado + pila.Pop();
+                pila.Pop();
+            }
+            else
+            {
+                pila.Push(infija[i]);
+            }
+
+        }
+        else
+        {
+            Resultado = Resultado + infija[i];
+        }
+
+
+    }
+    if (pila.Count == 1) Resultado = Resultado + pila.Pop();
+}
+
+
+
+Console.WriteLine(Resultado);
+
+
+static string ReverseString(string input)
+{
+    char[] charArray = input.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
---
+++ owo/Program.cs
@@ -0,0 +1,69 @@
+﻿//int n = int.Parse(Console.ReadLine());
+
+//for (int i = 0; i < n; i++)
+//{
+//    string case1 = Console.ReadLine();
+//    string case2 = Console.ReadLine();
+//    string salida ="";
+
+//    if (case1.Length > case2.Length)
+//    {
+//        for (int j = 0; j < case1.Length; j++)
+//        {
+//            if (j < case2.Length)
+//            {
+//                salida = salida + case1[j] + case2[j];
+//            }
+//            else
+//            {
+//                salida = salida + case1[j];
+//            }
+//            Console.WriteLine(salida);
+//        }
+
+
+//    }
+//    else
+//    {
+//        for (int k = 0; k < case2.Length; k++)
+//        {
+//            if (k < case1.Length)
+//            {
+//                salida = salida + case1[k] + case2[k];
+//            }
+//            else
+//            {
+//                salida = salida + case2[k];
+//            }
+//            Console.WriteLine(salida);
+//        }
+//    }
+
+//}
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string case1 = Console.ReadLine();
+    string case2 = Console.ReadLine();
+    string salida = "";
+
+    int maxLength = Math.Max(case1.Length, case2.Length);
+
+    for (int j = 0; j < maxLength; j++)
+    {
+        if (j < case1.Length)
+        {
+            salida += case1[j];
+        }
+
+        if (j < case2.Length)
+        {
+            salida += case2[j];
+        }
+
+        
+    }
+    Console.WriteLine(salida);
+}
